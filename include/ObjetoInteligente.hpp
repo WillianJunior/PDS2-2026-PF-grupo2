72a15088c08f230b53c9b640756ae5959ce4dcad
tiagobzztt <tiagobzztt@gmail.com>
2026-05-07 14:42:28 -0300

    hpps objetointeligente e sensor feitos (pendendo comentários)

--- include/ObjetoInteligente.hpp
+++ include/ObjetoInteligente.hpp
@@ -1,11 +1,41 @@
 #ifndef OBJETOINTELIGENTE_H
 #define OBJETOINTELIGENTE_H
+#include <vector>
+#include <string>
+#include <functional>
+#include "Sensor.hpp"
+
 
 class ObjetoInteligente {
 private:
+    bool restricaoAdulto;
+    std::vector<Sensor> sensores;
+    std::vector<std::string> statusPossiveis;
+    float consumoMedioDeEnergia;
+    std::vector<std::function<void()>> funcoes;
+    std::vector<std::function<void()>> funcoesRestritas;
 
 public:
-    
+    ObjetoInteligente(
+        bool restricaoAdulto,
+        std::vector<Sensor> sensores,
+        std::vector<std::string> statusPossiveis,
+        float consumoMedioDeEnergia,
+        std::vector<std::function<void()>> funcoes,
+        std::vector<std::function<void()>> funcoesRestritas
+    );
+
+    virtual ~ObjetoInteligente() = default;
+
+    void setRestricaoAdulto();
+    // void mover(Comodo& destino); responsabilidade da conta?
+    std::vector<Sensor> getSensores();
+    std::string getStatus();
+    void setStatusAtual(std::string status);
+    float getConsumoMedioDeEnergia();
+    std::vector<std::function<void()>> getFuncoes();
+    std::vector<std::function<void()>> getFuncoesRestritas();
+
 };
 
 #endif
--- include/Sensor.hpp
+++ include/Sensor.hpp
@@ -1,11 +1,26 @@
 #ifndef SENSOR_H
 #define SENSOR_H
+#include "ObjetoInteligente.hpp"
+#include "Modos"
+#include <vector>
 
 class Sensor {
 private:
+    bool estaLigado;
+    bool estaAtivado;
+    std::vector<ObjetoInteligente> objetosConectados;
+    std::vector<Modos> modosConectados;
 
 public:
     Sensor();
+    void setEstaLigado();
+    bool getEstaLigado();
+    void setEstaAtivado();
+    bool getEstaAtivado();
+
+    ObjetoInteligente getObjetosConectados();
+
+
 };
 
 #endif
