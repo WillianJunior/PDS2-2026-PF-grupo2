dd0b0d5ee6d79eb265a98346a10b295edaa6eaed
tiagobzztt <tiagobzztt@gmail.com>
2026-05-07 15:38:19 -0300

    repassando funções de conta para login, em comentário

---
+++ include/Login.hpp
@@ -0,0 +1,21 @@
+// // Métodos de Regra de Negócio (User Story 1 e 2)
+    
+//     /**
+//      * @brief Valida se a formatação do e-mail é válida (ex: contém '@' e '.').
+//      * @return true se for válido, false caso contrário.
+//      */
+//     bool validarEmailFormato() const;
+
+//     /**
+//      * @brief Verifica se a senha fornecida atende aos requisitos mínimos de segurança.
+//      * @return true se a senha for segura.
+//      */
+//     bool validarSenhaSegura() const;
+
+//     /**
+//      * @brief Autentica o usuário comparando a senha digitada com a senha armazenada.
+//      * @param senhaTentativa Senha digitada no login.
+//      * @return true se as senhas baterem.
+//      */
+//     bool autenticar(const std::string& senhaTentativa) const;
+    
