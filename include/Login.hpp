b201400df5bd6a31cd5a7ce150531a87d46fea40
MARIA PAULA BARBOSA VARTULI <mariapaulavartuli@ufmg.br>
2026-05-07 21:26:54 -0300

    Login.hpp pronto

--- include/Login.hpp
+++ include/Login.hpp
@@ -1,21 +1,52 @@
-// // Métodos de Regra de Negócio (User Story 1 e 2)
-    
-//     /**
-//      * @brief Valida se a formatação do e-mail é válida (ex: contém '@' e '.').
-//      * @return true se for válido, false caso contrário.
-//      */
-//     bool validarEmailFormato() const;
+#ifndef LOGIN_HPP
+#define LOGIN_HPP
 
-//     /**
-//      * @brief Verifica se a senha fornecida atende aos requisitos mínimos de segurança.
-//      * @return true se a senha for segura.
-//      */
-//     bool validarSenhaSegura() const;
+#include "Conta.hpp"
 
-//     /**
-//      * @brief Autentica o usuário comparando a senha digitada com a senha armazenada.
-//      * @param senhaTentativa Senha digitada no login.
-//      * @return true se as senhas baterem.
-//      */
-//     bool autenticar(const std::string& senhaTentativa) const;
-    
+#include <vector>
+#include <string>
+
+/**
+ * @class Login
+ * @brief Responsável pela criação de contas, bloquear/desbloquear contas e armazenar todas as contas cadastradas no sistema.
+ */
+
+ class Login{
+    private:
+        /**
+         * @brief Vetor que armazena as contas cadastradas.
+         */
+        std::vector<Conta> contas;
+
+    public:
+        /**
+         * @brief Construtor da classe Login.
+         * @param contasCadastradas Vetor inicial das contas já cadastradas.
+         */
+        Login(std::vector<Conta> contasCadastradas);
+        /**
+         * @brief Cadastra uma nova conta
+         * @param contaNova Conta que sera cadastrada.
+         */
+        void criarConta(Conta contaNova);
+        /**
+         * @brief Realiza autenticação de uma conta.
+         *  Procura o email e verifica se a senha fornecida está correta.
+         * @param email Email digitado.
+         * @param senha Senha digitada.
+         * @return true= autenticação válida.
+         */
+        bool autenticarConta(std::string email, std::string senha);
+        /**
+         * @brief Verifica se a conta está bloqueada
+         * true= conta bloqueada
+         */
+        bool bloquearConta(std::string email);
+        /**
+         * @brief Retorna as contas cadastradas.
+         * @return Vetor de contas.
+         */
+        std::vector<Conta> getContasCadastradas();
+
+    };
+#endif
