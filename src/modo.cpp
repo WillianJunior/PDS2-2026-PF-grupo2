860c081531e5c6b29428b778b844b7ce5e874337
MARIA PAULA BARBOSA VARTULI <mariapaulavartuli@ufmg.br>
2026-05-21 19:05:00 -0300

    Modo.cpp

---
+++ src/modo.cpp
@@ -0,0 +1,36 @@
+#include "Modo.hpp"
+
+Modo::Modo(
+    std::vector<ObjetoInteligente> objetos,
+    std::vector<Comodo> comodos,
+    bool ligado,
+    bool bloqueado
+)
+    : objetosRelacionados(objetos),
+      comodosRelacionados(comodos),
+      ligado(ligado),
+      bloqueado(bloqueado) {}
+
+void Modo::setAtivoModo(bool ativo) {
+    ligado = ativo;
+}
+
+bool Modo::getAtivoModo() {
+    return ligado;
+}
+
+void Modo::setBloqueadoModo(bool estado) {
+    bloqueado = estado;
+}
+
+bool Modo::getBloqueadoModo() {
+    return bloqueado;
+}
+
+std::vector<ObjetoInteligente> Modo::getObjetosRelacionados() {
+    return objetosRelacionados;
+}
+
+std::vector<Comodo> Modo::getComodosRelacionados() {
+    return comodosRelacionados;
+}
